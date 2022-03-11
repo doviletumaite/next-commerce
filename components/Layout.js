3aa83ac9a819bfbc627bedce3cc989a7abe52208
Dovile Tumaite <dovile.tumaite@yahoo.it>
2022-03-11 15:53:54 +0100

    style

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,24 @@
-import Head from 'next/head'
-import React from 'react'
-import { AppBar, Container, Toolbar, Typography } from "@material-ui/core"
+import Head from "next/head";
+import React from "react";
+import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
+import useStyle from "../utils/styles";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+  const classes = useStyle()
   return (
     <div>
-        <Head>
-            <title>MyCommerce</title>
-        </Head>
-        <AppBar position="static">
-         <Toolbar>
+      <Head>
+        <title>MyCommerce</title>
+      </Head>
+      <AppBar position="static" className={classes.navbar}>
+        <Toolbar>
           <Typography>MyCommerce</Typography>
-         </Toolbar>
-        </AppBar>
-        <Container>
-        {children}
-        </Container>
-        <footer>
-            <Typography>
-                All rights reserved. Next MyCommerce
-            </Typography>
-        </footer>
+        </Toolbar>
+      </AppBar>
+      <Container>{children}</Container>
+      <footer>
+        <Typography>All rights reserved. Next MyCommerce</Typography>
+      </footer>
     </div>
-  )
+  );
 }
